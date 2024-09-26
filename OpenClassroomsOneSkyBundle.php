8aeae4ae3511a38a841dea37338140db816992cc
Florian Hofinger <florian.hofinger@onlyfy.com>
2024-09-26 16:14:03 +0200

    refactor: fix return type

--- OpenClassroomsOneSkyBundle.php
+++ OpenClassroomsOneSkyBundle.php
@@ -4,6 +4,7 @@ namespace OpenClassrooms\Bundle\OneSkyBundle;
 
 use OpenClassrooms\Bundle\OneSkyBundle\DependencyInjection\OpenClassroomsOneSkyExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 /**
  * @author Romain Kuzniak <romain.kuzniak@openclassrooms.com>
@@ -13,7 +14,7 @@ class OpenClassroomsOneSkyBundle extends Bundle
     /**
      * {@inheritdoc}
      */
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new OpenClassroomsOneSkyExtension();
     }
